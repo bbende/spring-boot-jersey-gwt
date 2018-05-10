a94a72b7e9652308d883a9826c130186853c318d
Bryan Bende <bbende@gmail.com>
2018-05-10 07:50:01 -0400

    Incorporating gulp-sass into the build and customizing the primary color for bootstrap-material

--- notes-client/gulpfile.js
+++ notes-client/gulpfile.js
@@ -3,17 +3,33 @@ var gulpif = require('gulp-if');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
 var cleanCss = require('gulp-clean-css');
+var sass = require('gulp-sass');
 var util = require('gulp-util');
+var tildeImporter = require('node-sass-tilde-importer');
+
+// The tilde-importer is required to resolve imports like ~bootstrap/scss/...
+// The includePaths avoids the need to prefix all imports with the path to node_modules
+
+gulp.task('compile-sass', function() {
+    var srcDir = util.env.srcDir;
+
+    gulp.src(srcDir + '/scss/**/*.scss')
+        .pipe(sass({
+            includePaths: [ './node_modules' ],
+            importer: tildeImporter
+        }).on('error', sass.logError))
+        .pipe(gulp.dest(srcDir + '/css/'));
+});
+
+// Since the custom.scss will re-build bootstrap-material, we only need to include the css
+// directory from this project which will include custom.css built from custom.scss
 
 gulp.task('pack-css', function () {
     var srcDir = util.env.srcDir;
     var outDir = util.env.outDir;
     var minifyFlag = util.env.envName === 'prod';
 
-    return gulp.src([
-            'node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.css',
-            srcDir + '/css/**/*.css'
-        ])
+    return gulp.src(srcDir + '/css/**/*.css')
         .pipe(concat('stylesheet.css'))
         .pipe(gulpif(minifyFlag, cleanCss()))
         .pipe(gulp.dest(outDir + '/css'));
@@ -29,6 +45,7 @@ gulp.task('copy-index-html', function () {
 
 gulp.task('default',
     [
+        'compile-sass',
         'pack-css',
         'copy-index-html'
     ]);
